c530596b1a4f3c366d51634e025e15d45261b401
Hammed0630 <63410741+Hammed0630@users.noreply.github.com>
2020-06-17 12:03:49 -0400

    updated

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function processFirstItem(stringList, callback) {
  * 1. What is the difference between counter1 and counter2?
  * 
  * 2. Which of the two uses a closure? How can you tell?
- * counter 1 uses a closure
+ * counter 2 uses a closure using a call back
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
  *
 */
@@ -56,9 +56,13 @@ function counter2() {
 
 Write a function called `inning` that generates a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(){
-  Math.floor((Math.random() * 2) + 1);
+function inning(random){
+  let randomNum = Math.floor(Math.random() * 3)
+  random = randomNum
+  console.log(random)
+  return random
 }
+inning()
 
 
 /* Task 3: finalScore()
@@ -75,12 +79,21 @@ finalScore(inning, 9) might return:
 
 */ 
 
-function finalScore(/*code Here*/){
-
-  /*Code Here*/
-
+function finalScore(callBack, num){
+  let homeScore = 0
+  let awayScore = 0
+for(let i = 0; i <= num; i++){
+  homeScore  += callBack()
+  awayScore  += callBack()
 }
 
+return {
+  "home":homeScore,
+  "away":awayScore
+}
+  
+}
+console.log(finalScore(inning, 9))
 /* Task 4: 
 
 Create a function called `scoreboard` that accepts the following parameters: 
@@ -102,8 +115,17 @@ and returns the score at each pont in the game, like so:
 
 Final Score: 6 - 10 */
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard(callBack, num) {
+  let awayScore = 0
+  let homeScore = 0
+for(let i = 0; i <= num; i++){
+  homeScore  += callBack()
+  awayScore  += callBack()
+  console.log(`${i + 1} inning: ${homeScore} / ${awayScore} `)
 }
+console.log(` Final Score ${homeScore} / ${awayScore} `)
+}
+scoreboard(inning, 9)
+
 
 
